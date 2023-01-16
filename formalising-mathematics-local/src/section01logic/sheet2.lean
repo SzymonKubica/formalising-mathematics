be60a6641ca752b7d41359c306d7f7b4147b0d75
SzymonKubica <szymo.kubica@gmail.com>
2023-01-16 17:27:23 +0000

    Migrated file to a separate local repo to keep it separate from the course upstream one.

---
+++ formalising-mathematics-local/src/section01logic/sheet2.lean
@@ -0,0 +1,102 @@
+/-
+Copyright (c) 2022 Kevin Buzzard. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Author : Kevin Buzzard
+-/
+
+import tactic -- imports all the Lean tactics
+
+/-!
+
+# Logic in Lean, example sheet 2 : `true` and `false`
+
+We learn about the `true` and `false` propositions.
+
+## Tactics you will need
+
+To solve the levels on this sheet you will need to know all previous
+tactics, plus the following two new ones. Check out their explanations
+in the course book. Or just try them out and hover over them to see
+if you can understand what's going on.
+
+* `triv`
+* `exfalso`
+
+-/
+
+-- Throughout this sheet, `P`, `Q` and `R` will denote propositions.
+
+variables (P Q R : Prop)
+
+example : true :=
+begin
+  triv,
+end
+
+example : true → true :=
+begin
+  intro h,
+  exact h,
+end
+
+example : false → true :=
+begin
+  intro h,
+  exfalso,
+  exact h,
+end
+
+example : false → false :=
+begin
+  intro h,
+  exact h,
+end
+
+example : (true → false) → false :=
+begin
+  intro h1,
+  apply h1,
+  triv,
+end
+
+example : false → P :=
+begin
+  intro h,
+  exfalso,
+  exact h,
+end
+
+example : true → false → true → false → true → false :=
+begin
+  intro h1,
+  intro h2,
+  intro h3,
+  intro h4,
+  intro h5,
+  exact h2,
+end
+
+example : P → ((P → false) → false) :=
+begin
+  intro h1,
+  intro h2,
+  apply h2,
+  exact h1,
+end
+
+example : (P → false) → P → Q :=
+begin
+  intro h1,
+  intro h2,
+  exfalso,
+  apply h1,
+  exact h2,
+end
+
+example : (true → false) → P :=
+begin
+  intro h1,
+  exfalso,
+  apply h1,
+  triv,
+end
