93912a23138b7079211a40369e0e0a3b9cdc0b5f
SzymonKubica <szymo.kubica@gmail.com>
2023-01-18 09:31:20 +0000

    Started working on reals.

--- formalising-mathematics-local/src/section02reals/sheet5.lean
+++ formalising-mathematics-local/src/section02reals/sheet5.lean
@@ -12,7 +12,14 @@ import solutions.section02reals.sheet3 -- import the definition of `tends_to` fr
 theorem tends_to_neg {a : ℕ → ℝ} {t : ℝ} (ha : tends_to a t) :
   tends_to (λ n, - a n) (-t) :=
 begin
-  sorry,
+  rw tends_to at *,
+  have h : ∀ n, |a n - t| = | -a n - -t|,
+  intro,
+  ring,
+  rw abs_sub_comm,
+  rw add_comm,
+  ring,
+  simpa [h] using ha,
 end
 
 /-
@@ -23,7 +30,7 @@ try and suffer and struggle through the first principles proof.
 BIG piece of advice: write down a complete maths proof first,
 with all the details there. Then, once you know the maths
 proof, try translating it into Lean. Note that a bunch
-of the results we proved in sheet 4 will be helpful. 
+of the results we proved in sheet 4 will be helpful.
 -/
 
 /-- If `a(n)` tends to `t` and `b(n)` tends to `u` then `a(n) + b(n)`
@@ -32,7 +39,44 @@ theorem tends_to_add {a b : ℕ → ℝ} {t u : ℝ}
   (ha : tends_to a t) (hb : tends_to b u) :
   tends_to (λ n, a n + b n) (t + u) :=
 begin
-  sorry
+  rw tends_to at *,
+  have h1 : ∀ n, |a n + b n - (t + u)| = |(a n - t) + (b n - u) | :=
+  begin
+    intro,
+    ring_nf,
+  end,
+  simp [h1],
+  have h2 : ∀ n, |(a n - t) + (b n - u) | ≤ |a n - t| + |b n - u| :=
+  begin
+    intro,
+    exact abs_add (a n - t) (b n - u),
+  end,
+  intros ε hε,
+  have h3 : ∃ (B : ℕ), ∀ (n : ℕ), B ≤ n → |a n - t| < ε / 2 :=
+  begin
+    apply ha,
+    exact half_pos hε,
+  end,
+  have h4 : ∃ (B : ℕ), ∀ (n : ℕ), B ≤ n → |b n - u| < ε / 2 :=
+  begin
+    apply hb,
+    exact half_pos hε,
+  end,
+  have h5 : ∀ (n : ℕ), |b n - u| < ε / 2 ∧ |a n - t| < ε / 2 → |a n - t| + |b n - u| < ε :=
+  begin
+    intro,
+    intro h5,
+    cases h5,
+    linarith,
+  end,
+  cases h3,
+  cases h4,
+  use max h3_w h4_w,
+  intro,
+  intro h7,
+  have h8: h3_w ≤ n ∧ h4_w ≤ n,
+  rw max_le_iff at h7,
+  exact h7,
 end
 
 /-- If `a(n)` tends to t and `b(n)` tends to `u` then `a(n) - b(n)`
