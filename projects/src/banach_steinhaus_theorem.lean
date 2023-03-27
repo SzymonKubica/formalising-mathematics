b2d505687317d22921d23488d3f11f3e0c786c28
SzymonKubica <szymo.kubica@gmail.com>
2023-03-27 15:00:00 +0200

    Added the skeleton of the proof of Banach Steinhaus.

--- projects/src/banach_steinhaus_theorem.lean
+++ projects/src/banach_steinhaus_theorem.lean
@@ -3,6 +3,7 @@ import analysis.normed_space.operator_norm
 import tactic
 
 
+open normed_space
 
 /- In order to formulate the Banach-Steinhaus theorem, we need two normed spaces
    X and Y, we assume they are over two fields 𝔽₁, 𝔽₂ respectively. We also need
@@ -14,10 +15,37 @@ variables {X Y 𝔽₁ 𝔽₂ : Type*}
 [normed_space 𝔽₁ X] [normed_space 𝔽₂ Y]
 {σ : 𝔽₁ →+* 𝔽₂} [ring_hom_isometric σ]
 
+/- The following theorem needs to be proven first and then Banach-Steinhaus follows
+   from it as a corollary. -/
+theorem uniform_boundedness_principle {l : Type*}  (f : l → (X → ℝ))
+(h: ∀ x, ∃ K, ∀ (i : l), ‖f i x‖ ≤ K) (h_cont : ∀ (i : l), continuous (f i)) :
+∃ (x₀ : X) r, 0 < r ∧  ∃ K', ∀ (i : l) x ∈ metric.ball x₀ r, ‖f i x‖ ≤ K' :=
+begin
+ sorry,
+end
+
+
+#check continuous_linear_map.op_norm_le_bound
 
 theorem banach_steinhaus_theorem {l : Type*} [complete_space X] {f : l → (X →SL[σ] Y)}
   (h: ∀ x, ∃ K, ∀ (i : l), ‖f i x‖ ≤ K): ∃ K', ∀ i, ‖f i‖ ≤ K' :=
 begin
-  sorry,
+  /- Define a family of functions to which we'll apply the uniform boundedness principle. -/
+  let F : l → X → ℝ := λ i, (λ x, ‖f i x‖),
+  have h_cont: ∀ (i : l), continuous (F i),
+  { sorry },
+  have hF: ∀ x, ∃ K, ∀ (i : l), ‖F i x‖ ≤ K,
+  { sorry },
+  /- We get a ball B(x₀, r) from uniform boundedness_principle. -/
+  obtain ⟨x₀, ⟨r, hr, ⟨K', hK'⟩⟩⟩ := uniform_boundedness_principle F hF h_cont,
+  use K', -- we probably need to use some better bound.
+  intro i,
+  specialize hK' i,
+  apply continuous_linear_map.op_norm_le_bound,
+  { specialize hK' x₀,
+    exact le_trans (norm_nonneg (F i x₀)) (hK' (metric.mem_ball_self hr)), },
+  { intro x,
+    have hx : x = (‖x‖ / r) • (x₀ + ((r / ‖x‖) • x)- x₀),
+    },
 end
 
